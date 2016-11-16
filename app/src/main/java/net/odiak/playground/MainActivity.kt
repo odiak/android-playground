1152103710db1b692fe11bd2dee3dfcb4ef0b962
Kaido Iwamoto <h6824.kaido@gmail.com>
2016-11-16 17:00:41 +0900

    Impl sharing text and URL with chooser

--- app/src/main/java/net/odiak/playground/MainActivity.kt
+++ app/src/main/java/net/odiak/playground/MainActivity.kt
@@ -1,5 +1,6 @@
 package net.odiak.playground
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
@@ -7,5 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById(R.id.share)!!.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, "テスト http://google.com")
+            }
+            startActivity(
+                    Intent.createChooser(intent, "hoge")
+            )
+        }
     }
 }
